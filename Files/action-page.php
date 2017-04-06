db2fd88804a04ead77b8c6a84ae6fd51103c7f84
Lucario95 <luca_forza_samp@hotmail.it>
2017-04-06 10:08:27 +0200

    Prova php print

---
+++ Files/action-page.php
@@ -0,0 +1,7 @@
+<?php
+    $nome=$_POST['nome'];
+    $cognome=$_POST['cognome'];
+    $password=$_POST['password'];
+    $mail=$_POST['mail'];
+    print("$nome , $cognome , $password , $mail" );
+?>
